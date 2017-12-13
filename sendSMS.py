211aa1aa019e5e099a727642b92e23f27c4d0d2a
Carlos Montenegro <montenegro_carlosf@hotmail.com>
2017-12-13 01:27:58 -0600

    Got rid of token

--- sendSMS.py
+++ sendSMS.py
@@ -2,8 +2,8 @@ from twilio.rest import Client
 
 # Datos requeridos de la cuenta
 
-SID = "ACb5a0233d29cedb9ed60e9d51d227e541"
-auth_token = "e1701e1a644d10920e5d27acb54868b7"
+SID = ""
+auth_token = ""
 
 cliente = Client(SID, auth_token)
 
